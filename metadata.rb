79d115179e004b91b67deb807972b25761f6b7c0
Bipin Bachhao <bipinbachhao@gmail.com>
2020-12-21 17:41:52 -0500

    Initial commit

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'install_lmod'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
+maintainer 'Bipin Bachhao'
+maintainer_email 'bipinbachhao@gmail.com'
+license 'Apache 2.0'
 description 'Installs/Configures install_lmod'
 version '0.1.0'
 chef_version '>= 14.0'
@@ -10,10 +10,10 @@ chef_version '>= 14.0'
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/install_lmod/issues'
+# issues_url 'https://github.com/bipinbachhao/install_lmod/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/install_lmod'
+# source_url 'https://github.com/bipinbachhao/install_lmod'
